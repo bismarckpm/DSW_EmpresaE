b633eb79600f40c408e16907c9fb44b1051bcc54
juaneea <jeescobar.17@est.ucab.edu.ve>
2020-12-02 01:58:29 -0400

    Formularios y lista de Usuarios (Encuestados)
    Posibles errores en el formulario de registro (del login).
    Ademas recordar que usuario hace referencia a cualquier usuario, aqui se le llama usuario al Encuestado

--- Front/MercadoUcab/src/app/models/usuario.ts
+++ Front/MercadoUcab/src/app/models/usuario.ts
@@ -5,10 +5,7 @@ export interface Usuario {
       nombre2: string;
       apellido: string;
       apellido2: string;
-      email: string;
-      password: string;
-      rol: string;
-
-
+      estado: string;
+      fechaNacimiento: string;
 
  }

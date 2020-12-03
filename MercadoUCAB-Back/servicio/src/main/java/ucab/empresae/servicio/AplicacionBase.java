7b276629976fdb033023d5e7fc6b860cae381edb
Jairodm <57926143+Jairodm@users.noreply.github.com>
2020-12-03 17:29:09 -0400

    AplicacionBase en modulo servicio

---
+++ MercadoUCAB-Back/servicio/src/main/java/ucab/empresae/servicio/AplicacionBase.java
@@ -0,0 +1,8 @@
+package ucab.empresae.servicio;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath( "/api" )
+public class AplicacionBase extends Application
+{}

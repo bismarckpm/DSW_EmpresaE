c43373f22ddc9fe86fc3f79125c0c63c43e15d5d
juaneea <jeescobar.17@est.ucab.edu.ve>
2020-12-04 01:23:31 -0400

    Validaciones formularios Tipo

--- Front/MercadoUcab/src/app/componentes/form-tipo/form-tipo.component.ts
+++ Front/MercadoUcab/src/app/componentes/form-tipo/form-tipo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form-tipo',
@@ -7,15 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormTipoComponent implements OnInit {
 
-  item: any = {
-    descripcion: '',
-    nombre: '',
-    estado: ''
-  };
+  formTipo: FormGroup;
+  patronNombreTipo: any = /^[A-Za-z0-9\s]+$/;
 
-  constructor() { }
+  get nombreTipo(){
+    return this.formTipo.get('nombreTipo');
+  }
+
+  get descripcionTipo(){
+    return this.formTipo.get('descripcionTipo');
+  }
+
+  get estadoTipo(){
+    return this.formTipo.get('estadoTipo');
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm(){
+    this.formTipo = this.formBuilder.group({
+      nombreTipo: ['', [Validators.pattern(this.patronNombreTipo), Validators.required]],
+      descripcionTipo: ['', Validators.required],
+      estadoTipo: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.formTipo.valid) {
+      console.log(this.formTipo.value);
+    }
+    else{
+      alert('FILL ALL FIELDS');
+    }
   }
 
   agregarTipo(){

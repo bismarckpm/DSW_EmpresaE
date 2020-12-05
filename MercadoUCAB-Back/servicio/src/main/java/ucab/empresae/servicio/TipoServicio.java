c7b8d0a40cf76887ffe7bcfb9884c46f0aadfc5b
victorvieirap <victormvieira99@hotmail.com>
2020-12-05 00:49:54 -0400

    Update y Delete de Tipo

--- MercadoUCAB-Back/servicio/src/main/java/ucab/empresae/servicio/TipoServicio.java
+++ MercadoUCAB-Back/servicio/src/main/java/ucab/empresae/servicio/TipoServicio.java
@@ -1,6 +1,7 @@
 package ucab.empresae.servicio;
 
 
+import ucab.empresae.daos.Dao;
 import ucab.empresae.daos.DaoTipo;
 import ucab.empresae.dtos.DtoTipo;
 import ucab.empresae.entidades.TipoEntity;
@@ -63,6 +64,46 @@ public class TipoServicio {
         }else{
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+    }
+
+
+    @PUT
+    @Consumes(value=MediaType.APPLICATION_JSON)
+    @Produces(value=MediaType.APPLICATION_JSON)
+    @Path("/updateTipo/{id}")
+    public Response updateTipo(@PathParam("id") long id, DtoTipo dtoTipo) {
+        DaoTipo dao = new DaoTipo();
+        TipoEntity tipo = dao.find(id, TipoEntity.class);
+
+        if (tipo != null){
+            tipo.setNombre(dtoTipo.getNombre());
+            tipo.setEstado(dtoTipo.getEstado());
+            tipo.setDescripcion(dtoTipo.getDescripcion());
+            return Response.ok().entity(tipo).build();
+        }
+        else{
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @DELETE
+    @Produces(value=MediaType.APPLICATION_JSON)
+    @Path("deleteTipo/{id}")
+    public Response deleteTipo(@PathParam("id") long id)
+    {
+        try
+        {
+            DaoTipo dao = new DaoTipo();
+            TipoEntity tipo = dao.find(id, TipoEntity.class);
+            if(tipo != null) {
+                TipoEntity resul = dao.delete(tipo);
+                return Response.ok().build();
+            }
+        }
+        catch (Exception er){
+            String problema = er.getMessage();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
 }

2d7cdbc6cdaa51510614c98240119388f9c24c7e
juaneea <jeescobar.17@est.ucab.edu.ve>
2020-12-06 00:28:14 -0400

    CRUD tipo

--- Front/MercadoUcab/src/app/componentes/form-tipo/form-tipo.component.ts
+++ Front/MercadoUcab/src/app/componentes/form-tipo/form-tipo.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TipoService} from '../../services/tipo.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-form-tipo',
@@ -8,8 +10,29 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class FormTipoComponent implements OnInit {
 
+  @Input() tipo = {id: 0, nombre: '', estado: '', descripcion: ''};
   formTipo: FormGroup;
   patronNombreTipo: any = /^[A-Za-z0-9\s]+$/;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public tipoService: TipoService,
+    public router: Router) {
+    this.createForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  addTipo(tipo){
+    if (this.formTipo.valid) {
+      this.tipoService.createTipo(this.tipo).subscribe((data: {}) => {
+      });
+    }
+    else{
+      alert(' LLenar todos los campos por favor');
+    }
+  }
 
   get nombreTipo(){
     return this.formTipo.get('nombreTipo');
@@ -23,28 +46,12 @@ export class FormTipoComponent implements OnInit {
     return this.formTipo.get('estadoTipo');
   }
 
-  constructor(private formBuilder: FormBuilder) {
-    this.createForm();
-  }
-
   createForm(){
     this.formTipo = this.formBuilder.group({
       nombreTipo: ['', [Validators.pattern(this.patronNombreTipo), Validators.required]],
       descripcionTipo: ['', [Validators.pattern(this.patronNombreTipo), Validators.required]],
       estadoTipo: ['', Validators.required],
     });
-  }
-
-  ngOnInit(): void {
-  }
-
-  onSubmit() {
-    if (this.formTipo.valid) {
-      console.log(this.formTipo.value);
-    }
-    else{
-      alert('ES NECESARIO LLENAR LOS TODOS LOS CAMPOS');
-    }
   }
 
   agregarTipo(){
--- Front/MercadoUcab/src/app/services/tipo.service.ts
+++ Front/MercadoUcab/src/app/services/tipo.service.ts
@@ -1,44 +1,78 @@
 import { Injectable } from '@angular/core';
 import {Tipo} from '../models/tipo';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TipoService {
 
-  API_URI = 'https//';
-  tipos: Tipo[] = [
-    {
-      id: 0,
-      nombre: 'tipo2',
-      descripcion: 'tipo producto x',
-      estado: 'a',
-    },
-    {
-      id: 1,
-      nombre: 'tipo1',
-      descripcion: 'tipo producto y',
-      estado: 'i',
-    },
-  ];
+  apiurl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
-    getTipos(): Tipo[] {
-      return this.tipos;
-    }
+// Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
+  ///////// Metodos para ejecutar//////////////
+  getTipos(): Observable<Tipo[]>{
+    return this.http.get<Tipo[]>(this.apiurl + '/tipo')
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
 
-    getTipo(){
-      return this.http.get(`${this.API_URI}/tipo`);
-    }
+  getTipo(id): Observable<Tipo[]>{
+    return this.http.get<Tipo[]>(this.apiurl + '/tipo/' + id)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
 
-    registarTipo(tipo: Tipo){
-      return this.http.post(`${this.API_URI}/tipo`, tipo);
-    }
+  createTipo(Tipo): Observable<Tipo[]>{
+    return this.http.post<Tipo[]>(this.apiurl + '/tipo', JSON.stringify(Tipo), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
 
-    updateTipo(id , updateTipo){
-      return this.http.post(`${this.API_URI}/presentacion/${id}`, updateTipo);
+  updateTipo(id, Tipo): Observable<Tipo[]>{
+    return this.http.put<Tipo[]>(this.apiurl + '/tipo/' + id, JSON.stringify(Tipo), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
+
+  deleteTipo(id){
+    return this.http.delete<Tipo[]>(this.apiurl + '/tipo/' + id, this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
+
+///////////////////// Error HandleError
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
 }

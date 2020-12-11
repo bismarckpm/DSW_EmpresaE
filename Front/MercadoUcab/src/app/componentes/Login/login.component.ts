e0440539d0fef2fa973ef5cf5f69a45adb77aefb
Jose Manuel Gonzalez <33811865+Josestadele@users.noreply.github.com>
2020-12-10 22:16:26 -0400

    comint antes de traerme develp

--- Front/MercadoUcab/src/app/componentes/Login/login.component.ts
+++ Front/MercadoUcab/src/app/componentes/Login/login.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +12,52 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   
-  constructor() { 
-    
-  }
+
+  @Input() usuario:{username:'', clave:''}
+  formLogin: FormGroup;
+
+  constructor(
+    public usuarioService:UsuarioService,
+    private formBuilder: FormBuilder
+  ) { this.createForm() }
 
   ngOnInit(): void {
   }
+  
+  onSubmit() {
+    
+    if (this.formLogin.invalid) {
+        return;
+    }
+ 
+    this.usuarioService.Logear(this.usuario)
+        .pipe(first())
+        .subscribe(
+            error => {
+             //   this.alertService.error(error);
+            ///    this.loading = false;
+            });
+}
 
-  MandarForm(email,password){
-    alert('Enviando');
-    console.log(email);
-    console.log(password);
-  }
 
-  navegarHaciaOlvido(){
-    //this.router.navigate(['/Olvido']);
+logout() {
+  // remove user from local storage and set current user to null
+  localStorage.removeItem('currentUser');
+ // this.currentUserSubject.next(null);
+}
+
+
+
+  /// Validacion de Datos
+  get userName(){return this.formLogin.get('userName');}
+
+  get password(){return this.formLogin.get('password');}
+
+  createForm(){
+    this.formLogin = this.formBuilder.group({
+      userName: ['',Validators.required] ,
+      estadoMarca: ['', Validators.required],
+    });
   }
 
 }
--- Front/MercadoUcab/src/app/services/usuario.service.ts
+++ Front/MercadoUcab/src/app/services/usuario.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, map } from 'rxjs/operators';
 import {Usuario} from '../models/usuario';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+
+  private currentUserSubject: BehaviorSubject<Usuario>;
+  public currentUser: Observable<Usuario>;
+
+
   apiurl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+
+    this.currentUserSubject = new BehaviorSubject<Usuario>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 // Http Options
   httpOptions = {
     headers: new HttpHeaders({
@@ -60,6 +70,18 @@ export class UsuarioService {
       );
   }
 
+  Logear(usuario): Observable<Usuario[]>{
+      
+      return  this.http.post<Usuario[]>(this.apiurl+'/usuario/',JSON.stringify(usuario),this.httpOptions)
+      .pipe(map(user => {
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('currentUser', JSON.stringify(user));
+       // this.currentUserSubject.next(user);
+        return user;
+    }));
+
+  }
+
 ///////////////////// Error HandleError
   handleError(error) {
     let errorMessage = '';
@@ -73,4 +95,19 @@ export class UsuarioService {
     window.alert(errorMessage);
     return throwError(errorMessage);
   }
+
+  handleErrorLogin(error) {
+    let errorMessage = '';
+    if (error= invali) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+
 }

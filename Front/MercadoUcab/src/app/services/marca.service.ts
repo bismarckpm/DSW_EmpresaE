f58efedddd934bcf655255c4d81e8af1d5f325f6
Jose Manuel Gonzalez <33811865+Josestadele@users.noreply.github.com>
2020-12-12 20:57:48 -0400

    Preguntas con opciones ( Falta agregar como icono de informacion de la opciones que tiene relacionada esa pregunta )

--- Front/MercadoUcab/src/app/services/marca.service.ts
+++ Front/MercadoUcab/src/app/services/marca.service.ts
@@ -9,8 +9,8 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class MarcaService {
    //Definimos el url del api
-   apiurl='http://localhost:8080/servicio-1.0-SNAPSHOT/api';
-
+  // apiurl='http://localhost:8080/servicio-1.0-SNAPSHOT/api';
+  apiurl = 'http://localhost:3000';
   constructor(private http:HttpClient) { }
 
 // Http Options
--- Front/MercadoUcab/src/app/services/tipo.service.ts
+++ Front/MercadoUcab/src/app/services/tipo.service.ts
@@ -9,7 +9,8 @@ import {catchError, retry} from 'rxjs/operators';
 })
 export class TipoService {
 
-  apiurl = 'http://localhost:8080/servicio-1.0-SNAPSHOT/api';
+  //apiurl = 'http://localhost:8080/servicio-1.0-SNAPSHOT/api';
+  apiurl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 

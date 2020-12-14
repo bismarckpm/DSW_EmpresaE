a3ee88bfaccc8cf1241bfb355ecf984e1b528558
Omar David <oder1011119504@gmail.com>
2020-12-14 01:12:10 -0400

    update del sidebar

--- Front/MercadoUcab/src/app/componentes/sidebar/sidebar.component.ts
+++ Front/MercadoUcab/src/app/componentes/sidebar/sidebar.component.ts
@@ -12,27 +12,32 @@ import { AdminService } from '../../services/admin.service';
 export class SidebarComponent {
 
   secciones: string[] = [];
+  rol = '';
 
   constructor(
     private _adminService: AdminService,
     private encuestadoService: EncuestadoService,
-    private  analistaService: AnalistaService,
+    private analistaService: AnalistaService,
     private clienteService: ClienteService ) {
 
     if ((JSON.parse(localStorage.getItem('rol') )) == 'Administrador'){
       this.secciones = this._adminService.getSecciones();
+      this.rol = 'admin';
     }
 
     if ((JSON.parse(localStorage.getItem('rol') )) == 'Cliente'){
       this.secciones = this.clienteService.getSecciones();
+      this.rol = 'cliente';
     }
 
     if ((JSON.parse(localStorage.getItem('rol') )) == 'Encuestado'){
       this.secciones = this.encuestadoService.getSecciones();
+      this.rol = 'encuestado';
     }
 
     if ((JSON.parse(localStorage.getItem('rol') )) == 'Analista'){
       this.secciones = this.analistaService.getSecciones();
+      this.rol = 'analista';
     }
 
 

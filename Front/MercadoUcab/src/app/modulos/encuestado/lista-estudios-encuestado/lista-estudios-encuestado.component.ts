81bd018aa9b9f08f5fd3be968c8b16184a9650e6
Omar David <oder1011119504@gmail.com>
2020-12-24 15:16:31 -0400

    update diseño de responder encuesta

--- Front/MercadoUcab/src/app/modulos/encuestado/lista-estudios-encuestado/lista-estudios-encuestado.component.ts
+++ Front/MercadoUcab/src/app/modulos/encuestado/lista-estudios-encuestado/lista-estudios-encuestado.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {EstudioService} from '../../../services/estudio.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Estudio} from '../../../models/estudio';
+import {EstudioService} from '../../../services/estudio.service';
 import {EncuestaService} from '../../../services/encuesta.service';
-import {Encuesta} from '../../../models/encuesta';
 import { PreguntaService } from 'src/app/services/pregunta.service';
+import { Estudio } from '../../../models/estudio';
+import { Encuesta } from '../../../models/encuesta';
+import { Pregunta } from '../../../models/pregunta';
+import { TipoPregunta } from '../../../models/tipo-pregunta';
+import { Opcion } from '../../../models/opcion';
 
 @Component({
   selector: 'app-lista-estudios-encuestado',
@@ -15,16 +18,43 @@ import { PreguntaService } from 'src/app/services/pregunta.service';
 export class ListaEstudiosEncuestadoComponent implements OnInit {
 
   estudios: Estudio[] = [];
-  encuestas: Encuesta[] = [];
-  preguntasEncuesta: any = [];
-  opciones: any = [];
+  // encuestas: Encuesta[] = [];
+  preguntasEncuesta: Pregunta[] = [];
+  opciones: Opcion[] = [];
+  tipos: TipoPregunta[] = [];
   _id = this.actRoute.snapshot.params._id;
-  @Input() encuestadoData = {};
   formEstudioEncuestado: FormGroup;
+  presiono = 0;
+
+  // no sé para qué es este objeto
+  // @Input() encuestadoData = {};
+
+  // objeto individual, cada pregunta que sera agregada a la lista de respuesta
+  // esa lista de respuesta será agregada a JSON general en la clave "respuestas"
+  @Input() respuestaAPregunta: any = {
+    // id de la pregunta a responder
+    _id: 0,
+    respuesta: {
+      texto: '',
+      opciones: []
+    }
+  };
+
+  // varibale a asignar a la clave del objeto a devolver
+  listaRespuestas: any = [];
+  listaOpciones: any = [];
+
+  // objeto para registrar la opcion seleccionada
+  @Input() respuestaDOpcion = {_id: 0};
+
+  // objeto devolver en el post
+  respuestaEncuesta = {
+    respuestas: []
+  };
 
   constructor(
     public estudioService: EstudioService,
-    public encuestaService: EncuestaService,
+    // public encuestaService: EncuestaService,
     private preguntaService: PreguntaService,
     private formBuilder: FormBuilder,
     public actRoute: ActivatedRoute,
@@ -35,37 +65,71 @@ export class ListaEstudiosEncuestadoComponent implements OnInit {
     this.loadEstudios();
   }
 
+  addEncuesRespuestaEncuesta(): void{
+    this.respuestaEncuesta.respuestas = this.listaRespuestas;
+
+  }
+
   loadEstudios(): void{
-    let id = JSON.parse(localStorage.getItem('usuarioID'));
+    const id = JSON.parse(localStorage.getItem('usuarioID'));
     this.estudioService.getEstudioEncuestado(id).subscribe(data => {
       this.estudios = data;
     });
   }
 
   loadPreguntasResponder(idEstduio: number): void{
-    this.preguntaService.getPreguntasResponder(idEstduio).subscribe(data => {
-      this.preguntasEncuesta = data;
-    });
+    // aqui esta el get en preguntas pasandole el id del estudio
+    this.preguntaService.getPreguntasResponder(idEstduio)
+        .subscribe((data) => {
+          this.preguntasEncuesta = data;
+          this.extraerOpciones(0);
+        });
   }
 
-  extraerOpciones(idPregunta: number): void {
-    this.opciones = this.preguntasEncuesta[idPregunta].opciones;
-    console.log(this.opciones);
+  // posiblemente no se vaya a usar
+  // extraerOpciones(idPregunta: number): void {
+  //   this.opciones = this.preguntasEncuesta[idPregunta].opciones;
+  //   console.log(this.opciones);
+  // }
+
+  // capto las opciones que seleccionó el usuario
+  //  esta lista se agrega al objeto respuesta(individual)
+  addOpcionesRespuesta(idPregunta): void {
+    this.listaOpciones.push(this.respuestaDOpcion);
+    console.log('opcion seleccionada');
+    console.log(this.listaOpciones);
+    this.respuestaDOpcion = {_id: 0};
+    this.respuestaAPregunta._id = idPregunta;
+    this.respuestaAPregunta.respuesta.opciones = this.listaOpciones;
+    this.listaOpciones = [];
+    console.log('objeto pregunta');
+    console.log(this.respuestaAPregunta);
+    console.log('lista de preguntas a enviar');
+    this.listaRespuestas.push(this.respuestaAPregunta);
+    console.log(this.listaRespuestas);
+    this.respuestaAPregunta = {
+      _id: 0,
+      respuesta: {
+        texto: '',
+        opciones: []
+      }
+    };
   }
+
 
   // estos dos metodos no sé para qué son
-  loadEncuesta(): void{
-    let id = JSON.parse(localStorage.getItem('estudioID'));
-    this.encuestaService.getEncuestaEstudio(id).subscribe(data => {
-      this.encuestas = data;
-    });
-  }
+  // loadEncuesta(): void{
+  //   const id = JSON.parse(localStorage.getItem('estudioID'));
+  //   this.encuestaService.getEncuestaEstudio(id).subscribe(data => {
+  //     this.encuestas = data;
+  //   });
+  // }
 
-  loadPreguntas(): void{
-    let id = JSON.parse(localStorage.getItem('estudioID'));
-    this.encuestaService.getEncuestaEstudio(id).subscribe(data => {
-      this.encuestas = data;
-    });
-  }
+  // loadPreguntas(): void{
+  //   const id = JSON.parse(localStorage.getItem('estudioID'));
+  //   this.encuestaService.getEncuestaEstudio(id).subscribe(data => {
+  //     this.encuestas = data;
+  //   });
+  // }
 
 }

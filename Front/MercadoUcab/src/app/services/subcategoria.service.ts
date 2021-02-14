ec3273f94e9c6399cec729f7c099ec73f908d101
Jose Manuel Gonzalez <33811865+Josestadele@users.noreply.github.com>
2021-02-14 01:52:06 -0400

    Casi Listo Estudio , Error 500 en clienteEstudio 2

--- Front/MercadoUcab/src/app/services/subcategoria.service.ts
+++ Front/MercadoUcab/src/app/services/subcategoria.service.ts
@@ -12,6 +12,7 @@ export class SubcategoriaService {
   apiurl='http://localhost:8080/servicio-1.0-SNAPSHOT/api';
   //apiurl = 'http://localhost:3000';
 
+
   constructor(private http:HttpClient) { }
 // Http Options
   httpOptions = {
